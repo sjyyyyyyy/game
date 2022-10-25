0d76fa3d37d8c2a1d0698ef2bf72816f7d60267b
wuheng <admin@80boys.com>
2022-10-26 02:54:37 +0800

    优化代码

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,12 +2,12 @@ import axios from "axios"
 import { Respond } from "./types"
 
 export function pushUserPoint(user:string, point:number):void{
-    const getUrl = `http://order.80boys.com?username=${user}&point=${point}`
+    const getUrl = `http://order.80boys.com/pushUserPoint?username=${user}&point=${point}`
     axios.get(getUrl)
 }
 
 export async function getUserPoint():Promise<Respond>{
-    const getUrl = "http://order.80boys.com"
+    const getUrl = "http://order.80boys.com/getUserPoint"
     const {data} = await axios.get(getUrl)
     return data
 }
